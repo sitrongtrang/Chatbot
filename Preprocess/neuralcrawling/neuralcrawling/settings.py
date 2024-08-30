328bb474dae9cc9cbdf7a8f11d04f8d83ecc5e85
sitrongtrang <sitrongtrang@gmail.com>
2024-08-30 15:48:47 +0000

    saving

--- Preprocess/neuralcrawling/neuralcrawling/settings.py
+++ Preprocess/neuralcrawling/neuralcrawling/settings.py
@@ -15,6 +15,9 @@ NEWSPIDER_MODULE = "neuralcrawling.spiders"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "neuralcrawling (+http://www.yourdomain.com)"
+# settings.py
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
